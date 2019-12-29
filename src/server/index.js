20b06ed52c47c0be3ced649deb1819137b7b2b4a
Mikhail Metrikin <metrikin@gmail.com>
2019-12-29 13:54:00 -0800

    Started Webpack

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,17 @@
 import Koa from 'koa'
+import compress from 'koa-compress'
 
 const app = new Koa()
+
+app.use(compress({
+  // eslint-disable-next-line
+  filter(content_type) {
+    return /text/i.test(content_type)
+  },
+  threshold: 2048,
+  // eslint-disable-next-line
+  flush: require('zlib').Z_SYNC_FLUSH,
+}))
 
 app.use(async (ctx) => {
   ctx.body = 'Koa server started'
