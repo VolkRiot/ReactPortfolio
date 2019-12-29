1126defcc2e4aa349961188cba4c79ee4f28fb21
Mikhail Metrikin <metrikin@gmail.com>
2019-12-29 14:21:58 -0800

    Configured Webpack

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => (<div>Hello React, Webpack 4 & Babel 7!</div>)
+console.log('this runs')
 
 ReactDOM.render(<App />, document.querySelector('#root'))
